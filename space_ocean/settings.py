79da7eee201355d26d9d680dd8e75e27e13acb40
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-03-20 17:53:47 +1300

    Store base assets in /assets

--- space_ocean/settings.py
+++ space_ocean/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [path.join(BASE_DIR, 'static')]
+STATIC_ROOT = path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [path.join(BASE_DIR, 'assets')]
 
 LOGIN_REDIRECT_URL = 'logged-in'
 LOGOUT_REDIRECT_URL = 'thanks'
