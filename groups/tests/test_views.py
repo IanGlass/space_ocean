8d67ae6b3efd4dd5b2ac4df071c39ae7fa4be7c7
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-04-09 10:02:43 +1200

    Test coverage CreateGroup

--- groups/tests/test_views.py
+++ groups/tests/test_views.py
@@ -7,11 +7,25 @@ from groups.models import Group, GroupMember
 from groups.views import CreateGroup
 
 
+@pytest.fixture(autouse=True)
+def create_user():
+    global user
+    user = User.objects.create(username='Test User', password='Very Secure')
+
+
 @pytest.fixture()
 def create_group():
     yield Group.objects.create(name='Solar City', description='All topics solar')
 
     Group.objects.get(name='Solar City').delete()
+
+
+@pytest.fixture()
+def login(client):
+    client.force_login(user)
+    yield client
+
+    User.objects.get(username=user.username).delete()
 
 
 @pytest.mark.django_db
@@ -24,3 +38,16 @@ def test_list_groups(client):
 def test_single_group(client, create_group):
     response = client.get('/groups/details/' + create_group.slug)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_create_group(login):
+    response = login.post('/groups/new/', {
+        'name': 'Mars United',
+        'description': 'The United Federation of Mars'
+    })
+
+    assert response.status_code == 302
+
+    created_group = Group.objects.get(name='Mars United')
+    assert created_group.name == 'Mars United'
